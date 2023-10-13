662029e92785f7f9ae16952d31cfffaaebcf3c44
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-10-13 10:22:36 +0600

    added one extra section

--- components/HomePage/Home.jsx
+++ components/HomePage/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from './Banner/Banner'
+import Extra from './ExtraSection/Extra'
 import Faq from './Faq/Faq'
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <section className='w-full'>
       <Banner />
       <Faq />
+      <Extra />
     </section>
   )
 }
