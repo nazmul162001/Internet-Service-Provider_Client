79446e9e3d471d7fe225b9e6dc295f10163093dc
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-10-18 21:29:28 +0600

    fixed & customize accordion

--- src/components/Accordion/ManageAccordion.tsx
+++ src/components/Accordion/ManageAccordion.tsx
@@ -1,38 +1,50 @@
 import React from 'react'
-import type { CollapseProps } from 'antd'
-import { Collapse } from 'antd'
+import { BsPencil } from 'react-icons/bs'
+import { BiTrash } from 'react-icons/bi'
 
-const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`
-
-const items: CollapseProps['items'] = [
+const accordionData = [
   {
-    key: '1',
-    label: 'This is panel header 1',
-    children: <p className='mb-3'>{text}</p>,
+    id: 1,
+    title: 'What is Lorem Ipsum ?',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   },
   {
-    key: '2',
-    label: 'This is panel header 2',
-    children: <p className='mb-3'>{text}</p>,
-  },
-  {
-    key: '3',
-    label: 'This is panel header 3',
-    children: <p className='mb-3'>{text}</p>,
-  },
-  {
-    key: '3',
-    label: 'This is panel header 3',
-    children: <p className='mb-3'>{text}</p>,
+    id: 2,
+    title: 'Why do we use it ?',
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
   },
 ]
 
 const ManageAccordion = () => {
-  return <Collapse items={items} defaultActiveKey={['1']} />
+  const handleAccordionClick = (accordionId: any) => {
+    alert(`Accordion ID: ${accordionId}`)
+  }
+
+  return (
+    <div id='wrapper' className='custom_accordion'>
+      <ul>
+        {accordionData.map((accordion) => (
+          <li key={accordion.id} className='relative'>
+            <input type='checkbox' defaultChecked />
+            <i></i>
+            <h2>
+              {accordion.title}
+              <span className='flex items-center gap-2 absolute top-8 right-16 z-50'>
+                <BsPencil
+                  onClick={() => handleAccordionClick(accordion.id)}
+                  className='hover:text-green-500 '
+                />
+                <BiTrash className='hover:text-red-500 ' />
+              </span>
+            </h2>
+            <p>{accordion.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default ManageAccordion
--- src/components/HomePage/Faq/Accordion.tsx
+++ src/components/HomePage/Faq/Accordion.tsx
@@ -1,38 +1,64 @@
 import React from 'react'
-import type { CollapseProps } from 'antd'
-import { Collapse } from 'antd'
-
-const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`
-
-const items: CollapseProps['items'] = [
-  {
-    key: '1',
-    label: 'This is panel header 1',
-    children: <p className='mb-3'>{text}</p>,
-  },
-  {
-    key: '2',
-    label: 'This is panel header 2',
-    children: <p className='mb-3'>{text}</p>,
-  },
-  {
-    key: '3',
-    label: 'This is panel header 3',
-    children: <p className='mb-3'>{text}</p>,
-  },
-  {
-    key: '3',
-    label: 'This is panel header 3',
-    children: <p className='mb-3'>{text}</p>,
-  },
-]
 
 const Accordion = () => {
-  return <Collapse items={items} defaultActiveKey={['1']} />
+  return (
+    <div id='wrapper' className='custom_accordion'>
+      <ul>
+        <li>
+          <input type='checkbox' defaultChecked />
+          <i></i>
+          <h2>What is Lorem Ipsum ?</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </li>
+        <li>
+          <input type='checkbox' defaultChecked />
+          <i></i>
+          <h2>Why do we use it ?</h2>
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident, sometimes on purpose (injected humour
+            and the like).
+          </p>
+        </li>
+        <li>
+          <input type='checkbox' defaultChecked />
+          <i></i>
+          <h2>Where can we get it ?</h2>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable. If you are going to use a passage of Lorem Ipsum, you
+            need to be sure there isn't anything embarrassing hidden in the
+            middle of text. All the Lorem Ipsum generators on the Internet tend
+            to repeat predefined chunks as necessary, making this the first true
+            generator on the Internet. It uses a dictionary of over 200 Latin
+            words, combined with a handful of model sentence structures, to
+            generate Lorem Ipsum which looks reasonable. The generated Lorem
+            Ipsum is therefore always free from repetition, injected humour, or
+            non-characteristic words etc.
+          </p>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Accordion
