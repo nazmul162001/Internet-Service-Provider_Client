29aead9dda423cf5c05a1bae552d2074e75116b7
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-10-31 15:09:31 +0600

    update live link

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,3 @@
 // export const BASE_URL = 'http://localhost:5000'
-// old 
-// export const BASE_URL = 'https://internet-service-provider-backend.vercel.app'
+export const BASE_URL = 'https://internet-service-provider-backend.vercel.app'
 
-// new 
-export const BASE_URL = 'https://internet-service-provider-backend-g9cok1v5g.vercel.app/'
